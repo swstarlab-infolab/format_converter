87fd400915ce2d75bc88f42f422c773926a6c8fd
hellogaon <hellogaon@naver.com>
2022-04-27 17:27:48 +0900

    grid format을 edge label list로 변환: grid format내의 모든 shard를 순회하는 함수 추가

--- grid_to_el32/grid_to_el32.h
+++ grid_to_el32/grid_to_el32.h
@@ -18,7 +18,22 @@ public:
     ~grid_to_el32();
 
     bool run();
+    uint64_t get_total_edge();
     
 private:
     config_t _cfg;
+    std::string _input_path, _output_path;
+    uint32_t _row, _col;
+    uint32_t _row_grid_ID;
+    uint64_t _base_shard_size;
+    void* input_buf;
+    void* sparse_buf;
+    el32_ofstream* _el_ofs;
+    uint64_t _total_edge;
+
+    gstream::grid_format::grid_stream2 _gs;
+    bool _open_grid_stream();
+    
+    void _init_output_stream();
+    void _close_output_stream();
 };
