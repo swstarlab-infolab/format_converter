62a84001880279ae96ce5e189a06d4bc06385276
hellogaon <hellogaon@naver.com>
2022-04-27 17:28:51 +0900

    optimal을 확장자로 하는 입력 출력 파일 생성

--- gfstream/utility.h
+++ gfstream/utility.h
@@ -5,5 +5,9 @@
 
 bool el32_check_output_directory(std::string const& path, uint32_t row);
 void el32_remove_empty_output_directory(std::string const& path, uint32_t row);
+std::string el32_make_unsorted_path(std::string const& path, uint32_t const& row_grid_ID, uint32_t const& col_grid_ID);
+std::string el32_make_optimal_path(std::string const& path, uint32_t const& row_grid_ID, uint32_t const& col_grid_ID);
+std::string el32_make_subdir_path(std::string const& path, uint32_t const& row_grid_ID);
+std::string adj_make_path(std::string const& path, uint16_t const& file_number);
 
 #endif // GSTREAM_TOOL_GFSTREAM_UTILITY_H
